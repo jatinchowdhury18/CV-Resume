20c7c5644d630366766458f62cbde321fe58e382
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2019-06-23 12:52:20 -0700

    Add deploy script, fix typos

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "SSH-ing to server..."
+ssh jatin@ccrma-gate.stanford.edu <<'ENDSSH'
+
+cd Library/Web/about
+
+wget -O cv.pdf https://github.com/jatinchowdhury18/CV-Resume/raw/master/CV/cv.pdf
+
+ENDSSH
+
+echo "Deploy script completed."
