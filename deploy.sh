05e35eef888ea3183401511aff541dfa8cbbeb24
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2019-06-29 17:18:54 -0700

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,10 @@ ssh jatin@ccrma-gate.stanford.edu <<'ENDSSH'
 cd Library/Web/about
 
 rm cv.pdf
-
 wget -O cv.pdf https://github.com/jatinchowdhury18/CV-Resume/raw/master/CV/cv.pdf
+
+rm resume.pdf
+wget -O resume.pdf https://github.com/jatinchowdhury18/CV-Resume/raw/master/Resume/resume.pdf
 
 ENDSSH
 
